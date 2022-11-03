8a609661a2d4c33e0d5ef6a0497ba04e58d4be3b
AbdussamadYisau <sammieyisau@gmail.com>
2022-11-03 12:07:37 +0100

    Added React Router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import { useEffect, useState } from "react";
-import { Stories } from "./container/Stories";
+import { Link } from "react-router-dom";
 
 export const App = () => {
   return (
-   <Stories />
+    <div>
+      <h1>News Reader</h1>
+      <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem",
+        }}
+      >
+        <Link to="/stories">Stories</Link> 
+      
+      </nav>
+    </div>
   );
-}
+};
